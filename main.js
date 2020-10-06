71202b4a8b27564500e98ba4f6ad305232c9c70e
song changgeun <pekopeko11@gmail.com>
2020-10-06 23:57:40 +0900

    song

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+
+const callback = (result, status) => {
+  if (status === kakao.maps.services.Status.OK) {
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = result[0].address.address_name;
+  }
+};
+
+const getQueryStringObject = () => {
+  var a = window.location.search.substr(1).split('&');
+  if (a == '') return {};
+  var b = {};
+  for (var i = 0; i < a.length; ++i) {
+    var p = a[i].split('=', 2);
+    if (p.length == 1) b[p[0]] = '';
+    else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
+  }
+  return b;
+}
+
 // 카카오 지도 js
+
 const main = () => {
   // 리앤 통신
   const button = document.getElementById('send');
@@ -45,23 +66,5 @@ const main = () => {
     geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
   });
 
-  const callback = (result, status) => {
-    if (status === kakao.maps.services.Status.OK) {
-      var resultDiv = document.getElementById('result');
-      resultDiv.innerHTML = result[0].address.address_name;
-    }
-  };
-
-  const getQueryStringObject = () => {
-    var a = window.location.search.substr(1).split('&');
-    if (a == '') return {};
-    var b = {};
-    for (var i = 0; i < a.length; ++i) {
-      var p = a[i].split('=', 2);
-      if (p.length == 1) b[p[0]] = '';
-      else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
-    }
-    return b;
-  }
 };
 window.onload = main;
