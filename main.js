673506760f078610c3ea239e627fb592a750a130
song changgeun <pekopeko11@gmail.com>
2020-10-06 23:53:17 +0900

    song

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ const main = () => {
   // 리앤 통신
   const button = document.getElementById('send');
   button.onclick = () => {
-    const address = document.getElementById('result').innerText;
-    const lat = document.getElementById('lat').innerText;
-    const lng = document.getElementById('lng').innerText;
+    address = document.getElementById('result').innerText;
+    lat = document.getElementById('lat').innerText;
+    lng = document.getElementById('lng').innerText;
     const result = `${address},${lat},${lng}`;
     window.ReactNativeWebView.postMessage(result);
   };
+
   // 좌표 값 가져 오기
   const qs = getQueryStringObject();
   const lat = qs.lat; // 925641
   const lng = qs.lng; // 1666020
+  document.getElementById('lat').innerText = lat;
+  document.getElementById('lng').innerText = lng;
+  geocoder.coord2Address(lat, lng, callback)
   var container = document.getElementById('map');
   var options = {
-    center: new kakao.maps.LatLng(lat, lng),
+    center: new kakao.maps.LatLng(qs.lat, qs.lng),
     level: 3,
   };
   var map = new kakao.maps.Map(container, options);
@@ -28,6 +32,8 @@ const main = () => {
     position: map.getCenter(),
   });
   marker.setMap(map);
+
+  
   kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
     var latlng = mouseEvent.latLng;
     marker.setPosition(latlng);
@@ -36,15 +42,17 @@ const main = () => {
     document.getElementById('lat').innerText = lat;
     document.getElementById('lng').innerText = lng;
     var coord = new kakao.maps.LatLng(lat, lng);
-    var callback = function (result, status) {
-      if (status === kakao.maps.services.Status.OK) {
-        var resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = result[0].address.address_name;
-      }
-    };
     geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
   });
-  function getQueryStringObject() {
+
+  const callback = (result, status) => {
+    if (status === kakao.maps.services.Status.OK) {
+      var resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = result[0].address.address_name;
+    }
+  };
+
+  const getQueryStringObject = () => {
     var a = window.location.search.substr(1).split('&');
     if (a == '') return {};
     var b = {};
