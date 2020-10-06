6756cbf54490caed48da4e54f30b71ae128f0b60
song changgeun <pekopeko11@gmail.com>
2020-10-07 00:02:44 +0900

    song

--- main.js
+++ main.js
@@ -1,46 +1,27 @@
-
-const callback = (result, status) => {
-  if (status === kakao.maps.services.Status.OK) {
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = result[0].address.address_name;
-  }
-};
-
-const getQueryStringObject = () => {
-  var a = window.location.search.substr(1).split('&');
-  if (a == '') return {};
-  var b = {};
-  for (var i = 0; i < a.length; ++i) {
-    var p = a[i].split('=', 2);
-    if (p.length == 1) b[p[0]] = '';
-    else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
-  }
-  return b;
-}
-
 // 카카오 지도 js
-
 const main = () => {
   // 리앤 통신
   const button = document.getElementById('send');
   button.onclick = () => {
-    address = document.getElementById('result').innerText;
-    lat = document.getElementById('lat').innerText;
-    lng = document.getElementById('lng').innerText;
-    const result = `${address},${lat},${lng}`;
-    window.ReactNativeWebView.postMessage(result);
+    var lat = document.getElementById('lat').innerText;
+    var lng = document.getElementById('lng').innerText;
+    var coord = new kakao.maps.LatLng(lat, lng);
+    var callback = function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        var address = result[0].address.address_name;
+        var result = `${address},${lat},${lng}`;
+        window.ReactNativeWebView.postMessage(result);
+      }
+    };
+    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
   };
-
   // 좌표 값 가져 오기
   const qs = getQueryStringObject();
   const lat = qs.lat; // 925641
   const lng = qs.lng; // 1666020
-  document.getElementById('lat').innerText = lat;
-  document.getElementById('lng').innerText = lng;
-  geocoder.coord2Address(lat, lng, callback)
   var container = document.getElementById('map');
   var options = {
-    center: new kakao.maps.LatLng(qs.lat, qs.lng),
+    center: new kakao.maps.LatLng(lat, lng),
     level: 3,
   };
   var map = new kakao.maps.Map(container, options);
@@ -53,18 +34,24 @@ const main = () => {
     position: map.getCenter(),
   });
   marker.setMap(map);
-
-  
   kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
     var latlng = mouseEvent.latLng;
     marker.setPosition(latlng);
-    const lat = latlng.getLat();
-    const lng = latlng.getLng();
-    document.getElementById('lat').innerText = lat;
-    document.getElementById('lng').innerText = lng;
-    var coord = new kakao.maps.LatLng(lat, lng);
-    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+    document.getElementById('lat').innerText = latlng.getLat();
+    document.getElementById('lng').innerText = latlng.getLng();
   });
 
+  
+  function getQueryStringObject() {
+    var a = window.location.search.substr(1).split('&');
+    if (a == '') return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+      var p = a[i].split('=', 2);
+      if (p.length == 1) b[p[0]] = '';
+      else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
+    }
+    return b;
+  }
 };
 window.onload = main;
